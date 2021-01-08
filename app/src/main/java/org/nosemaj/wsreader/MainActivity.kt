23216e8a04c0b090584c6e1284f6419e1d2c0904
Jameson Williams <jhwill@amazon.com>
2021-01-08 04:37:37 -0600

    Handle errors more gracefully

--- app/src/main/java/org/nosemaj/wsreader/MainActivity.kt
+++ app/src/main/java/org/nosemaj/wsreader/MainActivity.kt
@@ -11,7 +11,7 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.features.websocket.WebSockets
 import io.ktor.client.features.websocket.wss
-import io.ktor.http.HttpMethod
+import io.ktor.http.HttpMethod.Companion.Get
 import io.ktor.http.Url
 import io.ktor.http.cio.websocket.Frame
 import io.ktor.http.cio.websocket.readText
@@ -20,6 +20,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.channels.consumeEach
 import kotlinx.coroutines.flow.Flow
@@ -45,46 +46,64 @@ class MainActivity : AppCompatActivity() {
             .map { binding.command.text.toString() }
         statusWindow = binding.status
         setContentView(binding.root)
-        setupWebSocket()
     }
 
-    private fun setupWebSocket() {
+    override fun onResume() {
+        super.onResume()
+        connect()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        lifecycleScope.cancel("Pausing activity")
+    }
+
+    private fun connect() {
         val url = Url(getString(R.string.websocket_url))
         val client = HttpClient(OkHttp) { install(WebSockets) }
         lifecycleScope.launch(Dispatchers.IO) {
-            client.wss(HttpMethod.Get, url.host, url.port, url.encodedPath) {
-                addStatus(R.string.websocket_connected, url.toString())
-                awaitAll(async {
-                    commands
-                        .onEach { addStatus(R.string.websocket_sent_command, it) }
-                        .map { command -> JSONObject()
-                            .put("action", "sendmessage")
-                            .put("data", command)
-                            .toString()
-                        }
-                        .collect { json -> outgoing.send(Frame.Text(json)) }
-                }, async {
-                    incoming.consumeEach {
-                        when (it) {
-                            is Frame.Text ->
-                                addStatus(R.string.websocket_received_message, it.readText())
-                            is Frame.Close ->
-                                addStatus(R.string.websocket_closed, closeReason.await()!!.message)
-                            else -> {}
-                        }
-                    }
-                })
+            try {
+                connect(client, url)
+            } catch (e: Throwable) {
+                addStatus(R.string.websocket_failure, e.message!!)
             }
         }
     }
 
+    private suspend fun connect(ktor: HttpClient, u: Url) {
+        ktor.wss(Get, u.host, u.port, u.encodedPath) {
+            addStatus(R.string.websocket_connected, u.toString())
+            awaitAll(async {
+                commands
+                    .onEach { addStatus(R.string.websocket_sent_command, it) }
+                    .map { command -> JSONObject()
+                        .put("action", "sendmessage")
+                        .put("data", command)
+                        .toString()
+                    }
+                    .collect { outgoing.send(Frame.Text(it)) }
+            }, async {
+                incoming.consumeEach {
+                    when (it) {
+                        is Frame.Text ->
+                            addStatus(R.string.websocket_received_message, it.readText())
+                        is Frame.Close ->
+                            addStatus(R.string.websocket_closed, closeReason.await()!!.message)
+                        else -> {}
+                    }
+                }
+            })
+        }
+    }
+
     private fun addStatus(@StringRes resId: Int, arg: String) {
-        val line = getString(R.string.status_line, lineNumber, getString(resId, arg))
+        val status = getString(resId, arg)
+        val line = getString(R.string.status_line, lineNumber, status)
         statusWindow.append(line)
         lineNumber++
     }
 
-    private fun View.clicks(): Flow<Unit> = callbackFlow {
+    private fun View.clicks() = callbackFlow {
         setOnClickListener {
             offer(Unit)
         }
