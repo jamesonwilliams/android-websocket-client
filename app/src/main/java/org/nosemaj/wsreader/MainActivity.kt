83c1f81014d1d5d6693a7af084913102baac864c
Jameson Williams <jhwill@amazon.com>
2021-01-08 00:21:25 -0600

    Re-implement WebSocket logic with Ktor

--- app/src/main/java/org/nosemaj/wsreader/MainActivity.kt
+++ app/src/main/java/org/nosemaj/wsreader/MainActivity.kt
@@ -1,66 +1,93 @@
 package org.nosemaj.wsreader
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.Response
-import okhttp3.WebSocket
-import okhttp3.WebSocketListener
+import androidx.lifecycle.lifecycleScope
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.features.websocket.WebSockets
+import io.ktor.client.features.websocket.wss
+import io.ktor.http.HttpMethod
+import io.ktor.http.Url
+import io.ktor.http.cio.websocket.Frame
+import io.ktor.http.cio.websocket.readText
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+
 import org.json.JSONObject
-import org.nosemaj.wsreader.R.string
 import org.nosemaj.wsreader.databinding.ActivityMainBinding
 
+@ExperimentalCoroutinesApi
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var ws: WebSocket
+    private lateinit var commands: Flow<String>
+    private lateinit var statusWindow: TextView
     private var lineNumber = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        commands = binding.button.clicks()
+            .map { binding.command.text.toString() }
+        statusWindow = binding.status
         setContentView(binding.root)
-        binding.button.setOnClickListener { sendCommand("turn_on_lights") }
         setupWebSocket()
     }
 
-    private fun sendCommand(@Suppress("SameParameterValue") command: String) {
-        ws.send(JSONObject()
-                .put("action", "sendmessage")
-                .put("data", command)
-                .toString())
-        appendStatus(getString(string.websocket_sent_command, command))
-    }
-
     private fun setupWebSocket() {
-        val request = Request.Builder()
-                .url(getString(string.websocket_url))
-                .build()
-        ws = OkHttpClient().newWebSocket(request, object: WebSocketListener() {
-            override fun onOpen(webSocket: WebSocket, response: Response) {
-                super.onOpen(webSocket, response)
-                appendStatus(getString(string.websocket_opened))
+        val url = Url(getString(R.string.websocket_url))
+        val client = HttpClient(OkHttp) { install(WebSockets) }
+        lifecycleScope.launch(Dispatchers.IO) {
+            client.wss(HttpMethod.Get, url.host, url.port, url.encodedPath) {
+                addStatus(R.string.websocket_connected, url.toString())
+                awaitAll(async {
+                    commands
+                        .onEach { addStatus(R.string.websocket_sent_command, it) }
+                        .map { command -> JSONObject()
+                            .put("action", "sendmessage")
+                            .put("data", command)
+                            .toString()
+                        }
+                        .collect { json -> outgoing.send(Frame.Text(json)) }
+                }, async {
+                    incoming.consumeEach {
+                        when (it) {
+                            is Frame.Text ->
+                                addStatus(R.string.websocket_received_message, it.readText())
+                            is Frame.Close ->
+                                addStatus(R.string.websocket_closed, closeReason.await()!!.message)
+                            else -> {}
+                        }
+                    }
+                })
             }
-
-            override fun onClosed(webSocket: WebSocket, code: Int, reason: String) {
-                super.onClosed(webSocket, code, reason)
-                appendStatus(getString(string.websocket_closed))
-            }
-
-            override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
-                super.onFailure(webSocket, t, response)
-                appendStatus(getString(string.websocket_failure, t.message))
-            }
-
-            override fun onMessage(webSocket: WebSocket, text: String) {
-                super.onMessage(webSocket, text)
-                appendStatus(getString(string.websocket_received_message, text))
-            }
-        })
+        }
     }
 
-    private fun appendStatus(message: String) {
-        binding.status.append(getString(string.status_line, lineNumber, message))
+    private fun addStatus(@StringRes resId: Int, arg: String) {
+        val line = getString(R.string.status_line, lineNumber, getString(resId, arg))
+        statusWindow.append(line)
         lineNumber++
+    }
+
+    private fun View.clicks(): Flow<Unit> = callbackFlow {
+        setOnClickListener {
+            offer(Unit)
+        }
+        awaitClose { setOnClickListener(null) }
     }
 }
